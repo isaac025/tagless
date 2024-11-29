5fb6c59bedc28d987b107dd4898d68c146c665ed
Isaac Hiram Lopez Diaz <isaac.lopez@upr.com>
2024-11-29 05:52:38 -0400

    made beta reduction

--- src/Initial.hs
+++ src/Initial.hs
@@ -1,20 +1,16 @@
 module Initial where
 
-import Control.Monad (when)
 import Data.Text.Lazy (Text, pack)
-import Data.Text.Lazy.IO (getLine, putStr)
-import System.Exit (exitSuccess)
-import Text.Parsec
+import Text.Parsec (
+    ParseError,
+    alphaNum,
+    char,
+    many1,
+    parse,
+    spaces,
+    (<|>),
+ )
 import Text.Parsec.Text.Lazy (Parser)
-import Prelude hiding (getLine, putStr)
-
-initialRepl :: IO ()
-initialRepl = do
-    putStr "> "
-    l <- getLine
-    when (l == "quit") exitSuccess
-    print $ parser l
-    initialRepl
 
 identity :: Expr
 identity = Lam "x" (Var "x")
@@ -53,3 +49,37 @@ parseExpr = parseApp <|> parseLam <|> parseVar
 
 parser :: Text -> Either ParseError Expr
 parser = parse parseExpr "initial"
+
+substitute :: Text -> Expr -> Expr -> Expr
+substitute x rep expr =
+    case expr of
+        Var v
+            | v == x -> rep
+            | otherwise -> Var v
+        Lam v body
+            | v == x -> Lam v body
+            | v `elem` freeVars rep ->
+                let v' = freshVar v body rep
+                 in Lam v' (substitute x rep (substitute v (Var v') body))
+        App e1 e2 -> App (substitute x rep e1) (substitute x rep e2)
+
+freeVars :: Expr -> [Text]
+freeVars expr =
+    case expr of
+        Var v -> [v]
+        Lam v body -> filter (/= v) (freeVars body)
+        App e1 e2 -> freeVars e1 ++ freeVars e2
+
+freshVar :: Text -> Expr -> Expr -> Text
+freshVar v e1 e2 = head [v' | v' <- map (\i -> v <> pack (show i)) [1 :: Int ..], v' `notElem` (freeVars e1 ++ freeVars e2)]
+
+betaReduce :: Expr -> Maybe Expr
+betaReduce expr =
+    case expr of
+        App (Lam x body) arg -> Just (substitute x arg body)
+        App e1 e2 ->
+            case betaReduce e1 of
+                Just e1' -> Just (App e1' e2)
+                Nothing -> App e1 <$> betaReduce e2
+        Lam x body -> Lam x <$> betaReduce body
+        _ -> Nothing
